3ec17c036b3a15e614e3720b6f511d23fa593c61
Youssef Messaoudi <youssef.messaoudi@epitech.eu>
2026-03-12 12:23:43 +0100

    Fix next.config.js remote patterns for images

--- next.config.js
+++ next.config.js
@@ -1,6 +1,33 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn-icons-png.flaticon.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'symfony.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.seeklogo.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'mariadb.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.mongodb.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'ghchart.rshah.org',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
